91ccec6e0e47c982d27c2118f6644ea3dec491e8
idev4u <us.idev.student@googlemail.com>
2015-07-23 21:39:44 +0200

    the Views are now connected to the right class files

---
+++ CentralInfoVC.h
@@ -0,0 +1,13 @@
+//
+//  CentralInfoVC.h
+//  haushaltsbuch
+//
+//  Created by Norman Sutorius on 23.07.15.
+//  Copyright © 2015 Norman Sutorius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CentralInfoVC : UIViewController
+
+@end
