6c05a8a5d230e5c97c81c1fe68053fdfd96e865e
idev4u <us.idev.student@googlemail.com>
2015-07-23 21:35:25 +0200

    Now the storyboard was created to show the input view

---
+++ haushaltsbuch/InputVCViewController.h
@@ -0,0 +1,13 @@
+//
+//  InputVCViewController.h
+//  haushaltsbuch
+//
+//  Created by Norman Sutorius on 23.07.15.
+//  Copyright © 2015 Norman Sutorius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InputVCViewController : UIViewController
+
+@end
